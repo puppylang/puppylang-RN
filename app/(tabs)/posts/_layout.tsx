bd61732ca0a97b30333eca87bca68a534997d647
hyebin <sksp0401@gmail.com>
2024-06-05 15:19:53 +0900

    Fix: post detail 수정

--- app/(tabs)/posts/_layout.tsx
+++ app/(tabs)/posts/_layout.tsx
@@ -1,10 +1,17 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs, useSegments } from "expo-router";
 
 export default () => {
+  const segments = useSegments();
+  const hidesTab = segments.includes("detail");
   return (
-    <Stack initialRouteName="home">
-      <Stack.Screen name="home" options={{ headerShown: false }} />
-      <Stack.Screen name="detail" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Tabs.Screen
+        options={{ tabBarStyle: { display: hidesTab ? "none" : "flex" } }}
+      />
+      <Stack initialRouteName="home">
+        <Stack.Screen name="home" options={{ headerShown: false }} />
+        <Stack.Screen name="detail" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 };
