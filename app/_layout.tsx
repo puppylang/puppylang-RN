99816a453ca54dd2f4fa72150f3ed88d60bfcda2
hyebin <sksp0401@gmail.com>
2024-06-05 15:19:42 +0900

    Fix: SafeAreaView props 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Layout() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 }} edges={["top", "right", "left"]}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
       </Stack>
