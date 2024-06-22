3f820267db52fef93da79f381d4545cce20e1bbd
hyebin <sksp0401@gmail.com>
2024-06-22 16:28:23 +0900

    Fix: StackHome 컴포넌트 적용

--- app/(tabs)/posts/home.tsx
+++ app/(tabs)/posts/home.tsx
@@ -1,31 +1,6 @@
-import { View } from "react-native";
-import WebView, { WebViewMessageEvent } from "react-native-webview";
-import { StackActions } from "@react-navigation/native";
-import { Tabs, useNavigation } from "expo-router";
-import { Route, WebviewRouter } from "../../../types/route";
+import StackHome from "../../../components/WebviewStackHome";
+import { Route } from "../../../types/route";
 
 export default function Post() {
-  const navigation = useNavigation();
-
-  const requestOnMessage = async (e: WebViewMessageEvent): Promise<void> => {
-    const nativeEvent = JSON.parse(e.nativeEvent.data) as WebviewRouter;
-    const { url } = nativeEvent;
-    const pushAction = StackActions.push("detail", {
-      url,
-      isStack: true,
-    });
-    navigation.dispatch(pushAction);
-  };
-
-  return (
-    <View style={{ flex: 1 }}>
-      <Tabs.Screen options={{ tabBarStyle: { display: "none" } }} />
-      <WebView
-        onMessage={requestOnMessage}
-        source={{
-          uri: Route.url + Route.posts,
-        }}
-      />
-    </View>
-  );
+  return <StackHome url={Route.posts} />;
 }
--- app/(tabs)/user/home.tsx
+++ app/(tabs)/user/home.tsx
@@ -1,29 +1,6 @@
-import { View } from "react-native";
-import WebView, { WebViewMessageEvent } from "react-native-webview";
-import { StackActions } from "@react-navigation/native";
-import { useNavigation } from "expo-router";
-
-import { Route, WebviewRouter } from "../../../types/route";
+import StackHome from "../../../components/WebviewStackHome";
+import { Route } from "../../../types/route";
 
 export default function User() {
-  const navigation = useNavigation();
-
-  const requestOnMessage = async (e: WebViewMessageEvent): Promise<void> => {
-    const nativeEvent = JSON.parse(e.nativeEvent.data) as WebviewRouter;
-    const { url } = nativeEvent;
-    const pushAction = StackActions.push("detail", {
-      url,
-      isStack: true,
-    });
-    navigation.dispatch(pushAction);
-  };
-
-  return (
-    <View style={{ flex: 1 }}>
-      <WebView
-        onMessage={requestOnMessage}
-        source={{ uri: Route.url + Route.user }}
-      />
-    </View>
-  );
+  return <StackHome url={Route.user} />;
 }
